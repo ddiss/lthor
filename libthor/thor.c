52ab758ac538fb2a3862774755953f43774a63c1
David Disseldorp <ddiss@samba.org>
2018-01-20 18:40:23 +0100

    libthor: add odin protocol helpers
    The helpers currently support PIT dumping, i.e. start session, request
    PIT, transfer PIT, end session/reboot.
    
    Signed-off-by: David Disseldorp <ddiss@samba.org>

--- libthor/thor.c
+++ libthor/thor.c
@@ -17,6 +17,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "thor.h"
 #include "thor_internal.h"
@@ -206,6 +207,224 @@ int thor_end_session(thor_device_handle *th)
 	ret = t_thor_exec_cmd(th, RQT_DL, RQT_DL_EXIT, NULL, 0);
 
 	return ret;
+}
+
+int thor_odin_start_session(thor_device_handle *th, uint32_t *_xfer_size)
+{
+	int ret;
+	struct rqt_odin_dl_init rqt = {0};
+	struct rsp_odin_dl_init rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_DL_INIT_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_DL_INIT;
+	rqt.subid = RQT_ODIN_DL_INIT_BEGIN;
+	/* xfer_size zero */
+
+	ret = rqt_odin_pack_dl_init(&rqt, buf, RQT_ODIN_PACKED_DL_INIT_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_DL_INIT_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_dl_init(buf, RSP_ODIN_PACKED_DL_INIT_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	if (rsp.xfer_size == 0) {
+		fprintf(stderr, "error: Loke xfer_size zero!\n");
+		return -EIO;
+	}
+
+	fprintf(stderr, "Odin session initialised with Loke xfer_size: %u\n",
+		rsp.xfer_size);
+	*_xfer_size = rsp.xfer_size;
+
+	return 0;
+}
+
+int thor_odin_end_session(thor_device_handle *th)
+{
+	int ret;
+	struct rqt_odin_dl_end rqt = {0};
+	struct rsp_odin_dl_end rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_DL_END_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_DL_END;
+	rqt.subid = RQT_ODIN_DL_END_REG;
+
+	ret = rqt_odin_pack_dl_end(&rqt, buf, RQT_ODIN_PACKED_DL_END_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_DL_END_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_DL_END_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_DL_END_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_dl_end(buf, RSP_ODIN_PACKED_DL_END_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin session ended with unknown: %u\n", rsp.unknown);
+
+	return 0;
+}
+
+int thor_odin_session_set_xfer_size(thor_device_handle *th,
+				    uint32_t xfer_size)
+{
+	int ret;
+	struct rqt_odin_dl_init rqt = {0};
+	struct rsp_odin_dl_init rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_DL_INIT_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_DL_INIT;
+	rqt.subid = RQT_ODIN_DL_INIT_XFER_SIZE;
+	rqt.xfer_size = xfer_size;
+
+	ret = rqt_odin_pack_dl_init(&rqt, buf, RQT_ODIN_PACKED_DL_INIT_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_DL_INIT_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_dl_init(buf, RSP_ODIN_PACKED_DL_INIT_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin session initialised xfer size to %u (rsp %u)\n",
+		xfer_size, rsp.xfer_size);
+
+	return 0;
+}
+
+int thor_odin_session_use_sd(thor_device_handle *th)
+{
+	int ret;
+	struct rqt_odin_dl_init rqt = {0};
+	struct rsp_odin_dl_init rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_DL_INIT_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_DL_INIT;
+	rqt.subid = RQT_ODIN_DL_INIT_TF;
+	/* xfer_size zero */
+
+	ret = rqt_odin_pack_dl_init(&rqt, buf, RQT_ODIN_PACKED_DL_INIT_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_DL_INIT_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_DL_INIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_dl_init(buf, RSP_ODIN_PACKED_DL_INIT_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin session initialised to use SD card (sz %u)\n",
+		rsp.xfer_size);
+
+	return 0;
+}
+
+int thor_odin_start_pit_dump(thor_device_handle *th, uint32_t *_dump_total)
+{
+	int ret;
+	struct rqt_odin_pit rqt = {0};
+	struct rsp_odin_pit rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_PIT_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_PIT;
+	rqt.subid = RQT_ODIN_PIT_DUMP;
+
+	ret = rqt_odin_pack_pit(&rqt, buf, RQT_ODIN_PACKED_PIT_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_PIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_PIT_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_PIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_pit(buf, RSP_ODIN_PACKED_PIT_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin pit dump initiated. total: %u\n", rsp.total_len);
+	*_dump_total = rsp.total_len;
+
+	return 0;
+}
+
+int thor_odin_end_pit_dump(thor_device_handle *th)
+{
+	int ret;
+	struct rqt_odin_pit rqt = {0};
+	struct rsp_odin_pit rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_PIT_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_PIT;
+	rqt.subid = RQT_ODIN_PIT_XFER_END;
+
+	ret = rqt_odin_pack_pit(&rqt, buf, RQT_ODIN_PACKED_PIT_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_PIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_PIT_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_PIT_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_pit(buf, RSP_ODIN_PACKED_PIT_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin pit dump ended. total: %u\n", rsp.total_len);
+
+	return 0;
 }
 
 static int t_thor_submit_chunk(struct t_thor_data_chunk *chunk)
@@ -531,6 +750,141 @@ int thor_send_data(thor_device_handle *th, struct thor_data_src *data,
 	return 0;
 }
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+static int thor_odin_recv_raw_data(thor_device_handle *th,
+				struct thor_data_src *data,
+				off_t trans_unit_size,
+				thor_progress_cb report_progress,
+				void *user_data)
+{
+	struct t_thor_data_transfer transfer_data;
+	uint8_t *buf;
+	int i, j;
+	int ret;
+
+	assert(th->odin_mode);
+
+	/* XXX FIXME transfer unit is always 500?! */
+	trans_unit_size = 500;
+
+	buf = malloc(trans_unit_size);
+	if (buf == NULL)
+		return -ENOMEM;
+
+	transfer_data.data = data;
+	transfer_data.report_progress = report_progress;
+	transfer_data.user_data = user_data;
+	transfer_data.data_left = data->get_file_length(data);
+	transfer_data.data_sent = 0;
+	transfer_data.chunk_number = 0;
+	transfer_data.completed = 0;		/* XXX unused */
+	transfer_data.data_in_progress = 0;	/* XXX unused */
+	transfer_data.ret = 0;
+
+	while (transfer_data.data_left > 0) {
+		struct rqt_odin_pit rqt = {0};
+		uint8_t rqt_buf[RQT_ODIN_PACKED_PIT_LEN];
+		off_t this_xfer = MIN(transfer_data.data_left, trans_unit_size);
+
+		rqt.id = RQT_ODIN_PIT;
+		rqt.subid = RQT_ODIN_PIT_PART;
+		rqt.part_off = transfer_data.chunk_number;
+
+		ret = rqt_odin_pack_pit(&rqt, rqt_buf, RQT_ODIN_PACKED_PIT_LEN);
+		if (ret < 0) {
+			transfer_data.ret = ret;
+			break;
+		}
+
+		ret = t_usb_send(th, rqt_buf, RQT_ODIN_PACKED_PIT_LEN,
+				DEFAULT_TIMEOUT);
+		if (ret < 0) {
+			transfer_data.ret = ret;
+			break;
+		}
+
+		transfer_data.chunk_number++;
+
+		ret = t_usb_recv(th, buf, this_xfer, DEFAULT_TIMEOUT);
+		if (ret < 0) {
+			transfer_data.ret = ret;
+			break;
+		}
+
+		transfer_data.data_left -= this_xfer;
+		transfer_data.data_sent += this_xfer;
+		ret = data->put_block(data, buf, this_xfer);
+		if (ret < 0) {
+			transfer_data.ret = ret;
+			break;
+		}
+
+		transfer_data.report_progress(transfer_data.th,
+					      transfer_data.data,
+					      transfer_data.data_sent,
+					      transfer_data.data_left,
+					      transfer_data.chunk_number,
+					      transfer_data.user_data);
+	}
+
+	/*
+	 * All done receiving PIT data.
+	 * XXX strangely, sometimes an empty bulk transfer is needed after
+	 * receiving, otherwise the PIT_END won't be processed.
+	 * - Galaxy Tab S2: required
+	 * - Galaxy S8: not needed
+	 */
+	t_usb_recv(th, buf, 0, 1);
+
+	free(buf);
+	return transfer_data.ret;
+}
+
+int thor_odin_recv_pit_data(thor_device_handle *th, uint32_t chunk_size,
+			    uint32_t dump_total, struct thor_data_src *data,
+			    enum thor_data_type type,
+			    thor_progress_cb report_progress,
+			    void *user_data,
+			    thor_next_entry_cb report_next_entry,
+			    void *ne_cb_data)
+{
+	off_t filesize;
+	const char *filename;
+	struct res_pkt resp;
+	off_t trans_unit_size;
+	int ret;
+
+	assert(type == THOR_PIT_DATA);
+	ret = data->next_file(data);
+	if (ret <= 0) {
+		fprintf(stderr, "invalid data dest\n");
+		return -EBADF;
+	}
+	if (report_next_entry)
+		report_next_entry(th, data, ne_cb_data);
+
+	if (!th) {
+		fprintf(stderr, "skipping chunk recv\n");
+		return 0;
+	}
+
+	filename = data->get_name(data);
+	ret = data->set_file_length(data, dump_total);
+	if (ret < 0) {
+		fprintf(stderr, "failed to truncate %s\n", filename);
+		return ret;
+	}
+
+	ret = thor_odin_recv_raw_data(th, data, chunk_size,
+				   report_progress, user_data);
+	if (ret < 0) {
+		fprintf(stderr, "failed to recv data for %s\n", filename);
+		return ret;
+	}
+
+	return 0;
+}
+
 int thor_reboot(thor_device_handle *th)
 {
 	int ret;
@@ -538,6 +892,40 @@ int thor_reboot(thor_device_handle *th)
 	ret = t_thor_exec_cmd(th, RQT_CMD, RQT_CMD_REBOOT, NULL, 0);
 
 	return ret;
+}
+
+int thor_odin_reboot(thor_device_handle *th)
+{
+	int ret;
+	struct rqt_odin_dl_end rqt = {0};
+	struct rsp_odin_dl_end rsp = {0};
+	uint8_t buf[RQT_ODIN_PACKED_DL_END_LEN];
+
+	assert(th->odin_mode);
+	rqt.id = RQT_ODIN_DL_END;
+	rqt.subid = RQT_ODIN_DL_END_REBOOT;
+
+	ret = rqt_odin_pack_dl_end(&rqt, buf, RQT_ODIN_PACKED_DL_END_LEN);
+	if (ret < 0)
+		return ret;
+
+	ret = t_usb_send(th, buf, RQT_ODIN_PACKED_DL_END_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	/* TODO: use build-time assert */
+	assert(RSP_ODIN_PACKED_DL_END_LEN <= sizeof(buf));
+	ret = t_usb_recv(th, buf, RSP_ODIN_PACKED_DL_END_LEN, DEFAULT_TIMEOUT);
+	if (ret < 0)
+		return ret;
+
+	ret = rsp_odin_unpack_dl_end(buf, RSP_ODIN_PACKED_DL_END_LEN, &rsp);
+	if (ret < 0)
+		return ret;
+
+	fprintf(stderr, "Odin reboot succeeded with unknown: %u\n", rsp.unknown);
+
+	return 0;
 }
 
 int thor_get_data_src(const char *path, enum thor_data_src_format format,
